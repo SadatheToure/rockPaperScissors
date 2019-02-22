e95a0179c7044e22a6b8e9df60501601d5fa6579
SadatheToure <trms75@gmail.com>
2019-02-21 21:46:55 -0500

    Scoreboard update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('score-board');
@@ -8,18 +8,63 @@ const rock_div = document.getElementById('r');
 const paper_div = document.getElementById('p');
 const scissors_div = document.getElementById('s');
 
-function game(userChoice) {
-  console.log('pop' + userChoice);
+function getComputerChoice() {
+  const choices = ['r', 'p', 's'];
+  const ramdomChoice = Math.floor(Math.random() * 3);
+  return choices[ramdomChoice];
 }
 
-rock_div.addEventListener('click', function () {
-  game('r');
-})
+function win() {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  console.log('USER WINS!')
+}
 
-paper_div.addEventListener('click', function () {
-  game('p');
-})
+function draw() {
+  console.log('IT\'S ADRAW!')
+}
 
-scissors_div.addEventListener('click', function () {
-  game('s');
-})
+function lose() {
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  console.log('USER LOSES!')
+}
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case 'rs':
+    case 'pr':
+    case 'sp':
+      win();
+      break;
+    case 'rp':
+    case 'ps':
+    case 'sr':
+      lose();
+      break;
+    case 'rr':
+    case 'pp':
+    case 'ss':
+      draw();
+      break;
+  }
+}
+
+function main() {
+  rock_div.addEventListener('click', function () {
+    game('r');
+  })
+
+  paper_div.addEventListener('click', function () {
+    game('p');
+  })
+
+  scissors_div.addEventListener('click', function () {
+    game('s');
+  })
+}
+
+main();
